18feae43428184626f8ccc81f71b9f83d23838be
aliiyjm <2043ds130@utez.edu.x>
2025-11-19 20:52:41 -0600

    Integración con DataStore para manejar el volumen

--- app/src/main/java/mx/edu/utez/grabadormultimedia/ViewModel/PlaybackViewModel.kt
+++ app/src/main/java/mx/edu/utez/grabadormultimedia/ViewModel/PlaybackViewModel.kt
@@ -7,10 +7,13 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.media3.exoplayer.ExoPlayer
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class PlaybackViewModel(
     application: Application,
@@ -27,3 +30,25 @@ class PlaybackViewModel(
     private var accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     private val _isAccelerometerEnabled = MutableStateFlow(false)
     val isAccelerometerEnabled: StateFlow<Boolean> = _isAccelerometerEnabled.asStateFlow()
+
+    // --- DataStore (Volumen) ---
+    val currentVolume: StateFlow<Float> = settingsRepository.userVolume
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = SettingsRepository.DEFAULT_VOLUME
+        )
+    init {
+// Observar el volumen de DataStore y aplicarlo a ExoPlayer
+        viewModelScope.launch {
+            currentVolume.collect { volume ->
+                exoPlayer.volume = volume
+            }
+        }
+// Observar el estado de reproducción de ExoPlayer
+        exoPlayer.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                _isPlaying.value = isPlaying
+            }
+        })
+    }
