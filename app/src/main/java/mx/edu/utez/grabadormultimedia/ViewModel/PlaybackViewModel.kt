28a7bf415a06850bbca4816ab05a2806a6fc5854
aliiyjm <2043ds130@utez.edu.x>
2025-11-19 20:51:06 -0600

    Configuración e implementación del acelerómetro

--- app/src/main/java/mx/edu/utez/grabadormultimedia/ViewModel/PlaybackViewModel.kt
+++ app/src/main/java/mx/edu/utez/grabadormultimedia/ViewModel/PlaybackViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.AndroidViewModel
 import androidx.media3.exoplayer.ExoPlayer
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,3 +20,10 @@ class PlaybackViewModel(
     val exoPlayer: ExoPlayer = ExoPlayer.Builder(application).build()
     private val _isPlaying = MutableStateFlow(false)
     val isPlaying: StateFlow<Boolean> = _isPlaying.asStateFlow()
+
+    // --- Sensores (Acelerómetro) ---
+    private val sensorManager: SensorManager =
+        getSystemService(application, SensorManager::class.java) as SensorManager
+    private var accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    private val _isAccelerometerEnabled = MutableStateFlow(false)
+    val isAccelerometerEnabled: StateFlow<Boolean> = _isAccelerometerEnabled.asStateFlow()
