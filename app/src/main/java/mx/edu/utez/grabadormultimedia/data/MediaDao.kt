ddd7530bfdb8b4a57888952466d88bc5b7af9165
Sam6482 <20243ds141@utez.edu.mx>
2025-11-19 21:16:02 -0600

    Creando MediaDao.kt

---
+++ app/src/main/java/mx/edu/utez/grabadormultimedia/data/MediaDao.kt
@@ -0,0 +1,18 @@
+package mx.edu.utez.grabadormultimedia.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MediaDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMedia(item: MediaItem)
+
+    @Query("SELECT * FROM media_items WHERE type = :type ORDER BY date DESC")
+    fun getMediaByType(type: MediaType): Flow<List<MediaItem>>
+}
+
