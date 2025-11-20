56d03d1a1edfc2d18938c20247d4317911626bad
aliiyjm <2043ds130@utez.edu.x>
2025-11-19 20:56:47 -0600

    Finalizacion del PlaybackViewModelFactory.kt

---
+++ app/src/main/java/mx/edu/utez/grabadormultimedia/ViewModel/PlaybackViewModelFactory.kt
@@ -0,0 +1,16 @@
+package mx.edu.utez.grabadormultimedia.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+
+class PlaybackViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PlaybackViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            val repository = SettingsRepository(application)
+            return PlaybackViewModel(application, repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
