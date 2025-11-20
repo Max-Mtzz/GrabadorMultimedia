6d1f60f30a90cd89c8b3b1ab3195db4edf6d47f1
aliiyjm <2043ds130@utez.edu.x>
2025-11-19 21:14:27 -0600

    Configurar navegación completa de la app con inyección de ViewModels y rutas composables

---
+++ app/src/main/java/mx/edu/utez/grabadormultimedia/ui/AppNavigation.kt
@@ -0,0 +1,78 @@
+package mx.edu.utez.grabadormultimedia.ui
+
+import android.app.Application
+import android.net.Uri
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import mx.edu.utez.grabadormultimedia.ViewModel.PlaybackViewModel
+import mx.edu.utez.grabadormultimedia.ViewModel.PlaybackViewModelFactory
+import mx.edu.utez.grabadormultimedia.ui.screens.RecordingScreen
+import mx.edu.utez.grabadormultimedia.ui.theme.AppBottomNavBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+    val application = context.applicationContext as Application
+// Factories para los ViewModels
+    val mediaViewModel: MediaViewModel = viewModel(
+        factory = MediaViewModelFactory(application)
+    )
+    val playbackViewModel: PlaybackViewModel = viewModel(
+        factory = PlaybackViewModelFactory(application)
+    )
+    Scaffold(
+        bottomBar = {
+            AppBottomNavBar(navController = navController)
+        }
+    ) { padding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Recording.route,
+            modifier = Modifier.padding(padding)
+        ) {
+            composable(Screen.Recording.route) {
+                RecordingScreen(mediaViewModel = mediaViewModel)
+            }
+            composable(Screen.AudioList.route) {
+                AudioListScreen(
+                    mediaViewModel = mediaViewModel,
+                    playbackViewModel = playbackViewModel
+                )
+            }
+            composable(Screen.ImageList.route) {
+                ImageListScreen(mediaViewModel = mediaViewModel)
+            }
+
+            composable(Screen.VideoList.route) {
+                VideoListScreen(
+                    mediaViewModel = mediaViewModel,
+                    navController = navController
+                )
+            }
+            composable(
+                route = Screen.VideoPlayer.route,
+                arguments = listOf(navArgument("uri") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val uri = backStackEntry.arguments?.getString("uri")
+                if (uri != null) {
+                    VideoPlayerScreen(
+                        uri = Uri.decode(uri),
+                        playbackViewModel = playbackViewModel
+                    )
+                }
+            }
+        }
+    }
+}
